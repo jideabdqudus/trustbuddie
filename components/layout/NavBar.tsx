7970f7274055ce05d673ab2cccf672969d4e302e
jideabdqudus <jideabdqudus@gmail.com>
2021-07-11 03:01:02 +0100

    Navbar done

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-
-export const NavBar = () => {
-  return <div>Navbar here</div>;
+import {Button, Col, Row} from "antd"
+export const NavBar: React.FC = () => {
+  return (
+    <div className="header">
+  <Row className="header__bg">
+    <Col span={12}><h3 className="header__logo">TrustBuddie</h3></Col>
+    <Col span={3}><span>Categories</span></Col>
+    <Col span={2}><span>Plans</span></Col>
+    <Col span={2}><span>Blog</span></Col>
+    <Col span={2}><span>Log in</span></Col>
+    <Col span={3}><Button className="btn-primary">Write a review</Button></Col>
+  </Row>
+    </div>
+  );
 };
