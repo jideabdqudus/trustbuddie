2d14959e12be485e0f95bd45b3ce1cb09d48035c
jideabdqudus <jideabdqudus@gmail.com>
2021-07-25 09:17:11 +0100

    middle hero

--- components/Content.tsx
+++ components/Content.tsx
@@ -15,18 +15,18 @@ export const Content: React.FC = () => {
   return (
     <div className="middle-hero">
       <div className="middle-hero">
-        <Row style={{marginBottom:"120px"}}>
+        <Row style={{marginBottom:"80px"}}>
           <Col span={12} className="middle-hero__left">
             <Image src={Pos} alt="seamless payment" className="middle-hero__left__image"/>
           </Col>
           <Col span={12} className="middle-hero__right">
-            <div style={{marginBottom:"35px"}}>
+            <div style={{margin:"40px 20px 35px 0"}}>
               <h1>Bridging the gap between customers and businesses</h1>
               <span>We are a technology company focused on</span>
             </div>
             <Row gutter={[32, 32]}>
               <Col span={12}>
-              <Image src={Stars} alt="Stars" className="middle-hero__right__image" />
+                  <Image src={Stars} alt="Stars" className="middle-hero__right__image" />
                   <h3> Trust</h3>
                   <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed di nonum eirmod tempor invidu labore.</h5>
               </Col>
@@ -39,10 +39,10 @@ export const Content: React.FC = () => {
             <Button className="btn-primary__content">Write A Review</Button>
           </Col>
         </Row>
-        <Row>
+        <Row style={{marginBottom:"10px"}}>
         <Col span={12} className="middle-hero__right">
-            <div style={{marginBottom:"35px", paddingRight:"80px"}}>
-              <h1>We are committed to helping you make more informed choices</h1>
+            <div style={{margin:"35px 0px 35px 0px", paddingRight:"80px"}}>
+              <h1 style={{marginBottom:"10px"}}>We are committed to helping you make more informed choices</h1>
               <span>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nemi eirmod labore et dolore magna aliquyam erat sed diam nonumy eirtr tempor invidunt ut labore et dolore.consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.</span>
             </div>
             <Row gutter={[0, 16]}>
@@ -60,11 +60,11 @@ export const Content: React.FC = () => {
             <Image src={Chart} alt="chart" className="middle-hero__left__image"/>
           </Col>
           <Col span={12} className="middle-hero__right">
-            <div style={{marginBottom:"35px"}}>
+            <div style={{margin:"55px 0px 35px 0px"}}>
               <h1>Over 71% of Nigerians believe customer reviews are helpful</h1>
               <span>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nemi eirmod labore et dolore magna aliquyam erat sed diam nonumy eirtr tempor invidunt ut labore et dolore.consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut.</span>
             </div>
-            <Button className="btn-primary__content">Write A Review</Button>
+            <Button className="btn-primary__underlined">Learn More > </Button>
           </Col>
         </Row>
       </div>
